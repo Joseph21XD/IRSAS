e38607d12c388fea7089aef9e4b12014831663bf
Eduardo Jirón <emjironal@gmail.com>
2019-11-24 20:50:05 -0600

    Cambios base de datos TEMPRESPUESTAFORM

--- Insercion-nuevos-datos/insert-data.sql
+++ Insercion-nuevos-datos/insert-data.sql
@@ -1,14 +1,19 @@
-LOAD DATA LOCAL INFILE '/run/media/eduardo/Datos/Otros/Asistencia_proyecto/IRSAS/Insercion-nuevos-datos/Asadas.csv' INTO TABLE ASADA
+LOAD DATA LOCAL INFILE '../Insercion-nuevos-datos/Asadas.csv' INTO TABLE ASADA
   FIELDS TERMINATED BY ','
   OPTIONALLY ENCLOSED BY '"'
   LINES TERMINATED BY '\n' IGNORE 1 rows;
 
-LOAD DATA LOCAL INFILE '/run/media/eduardo/Datos/Otros/Asistencia_proyecto/IRSAS/Insercion-nuevos-datos/AsadasInfo.csv' INTO TABLE ASADAINFO
+LOAD DATA LOCAL INFILE '../Insercion-nuevos-datos/AsadasInfo.csv' INTO TABLE ASADAINFO
   FIELDS TERMINATED BY ','
   OPTIONALLY ENCLOSED BY '"'
   LINES TERMINATED BY '\n' IGNORE 1 rows (Asada_ID, Ubicacion, Telefono, Poblacion, Url,cantAbonados);
 
-LOAD DATA LOCAL INFILE '/run/media/eduardo/Datos/Otros/Asistencia_proyecto/IRSAS/Insercion-nuevos-datos/INDICADORXASADA.csv' INTO TABLE INDICADORXASADA
+LOAD DATA LOCAL INFILE '../Insercion-nuevos-datos/INDICADORXASADA.csv' INTO TABLE INDICADORXASADA
   FIELDS TERMINATED BY ','
   OPTIONALLY ENCLOSED BY '"'
   LINES TERMINATED BY '\n' IGNORE 1 rows (Año,Indicador_id,Asada_id,Valor,Texto);
+  
+LOAD DATA LOCAL INFILE '../Datos/Usuarios.csv' INTO TABLE USUARIO
+  FIELDS TERMINATED BY ','
+  OPTIONALLY ENCLOSED BY '"'
+  LINES TERMINATED BY '\n' IGNORE 1 rows (Nombre, USUARIO, Contrasenna, Tipo);
